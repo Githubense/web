07eacec42bddda09467ca9602cc1c65d04232d08
Githubense <angelopf@outlook.com>
2025-06-24 09:46:59 +0200

    ewfhjlbidn

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ import {
 
 // import Link from 'next/link'
 // import { AnimatedBackground } from '@/components/ui/animated-background'
-
 import {
   PROJECTS,
   // WORK_EXPERIENCE,
@@ -51,51 +50,51 @@ type ProjectVideoProps = {
   src: string
 }
 
-function ProjectVideo({ src }: ProjectVideoProps) {
-  return (
-    <MorphingDialog
-      transition={{
-        type: 'spring',
-        bounce: 0,
-        duration: 0.3,
-      }}
-    >
-      <MorphingDialogTrigger>
-        <video
-          src={src}
-          autoPlay
-          loop
-          muted
-          className="aspect-video w-full cursor-zoom-in rounded-xl"
-        />
-      </MorphingDialogTrigger>
-      <MorphingDialogContainer>
-        <MorphingDialogContent className="relative aspect-video rounded-2xl bg-zinc-50 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950 dark:ring-zinc-800/50">
-          <video
-            src={src}
-            autoPlay
-            loop
-            muted
-            className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
-          />
-        </MorphingDialogContent>
-        <MorphingDialogClose
-          className="fixed top-6 right-6 h-fit w-fit rounded-full bg-white p-1"
-          variants={{
-            initial: { opacity: 0 },
-            animate: {
-              opacity: 1,
-              transition: { delay: 0.3, duration: 0.1 },
-            },
-            exit: { opacity: 0, transition: { duration: 0 } },
-          }}
-        >
-          <XIcon className="h-5 w-5 text-zinc-500" />
-        </MorphingDialogClose>
-      </MorphingDialogContainer>
-    </MorphingDialog>
-  )
-}
+// function ProjectVideo({ src }: ProjectVideoProps) {
+//   return (
+//     <MorphingDialog
+//       transition={{
+//         type: 'spring',
+//         bounce: 0,
+//         duration: 0.3,
+//       }}
+//     >
+//       <MorphingDialogTrigger>
+//         <video
+//           src={src}
+//           autoPlay
+//           loop
+//           muted
+//           className="aspect-video w-full cursor-zoom-in rounded-xl"
+//         />
+//       </MorphingDialogTrigger>
+//       <MorphingDialogContainer>
+//         <MorphingDialogContent className="relative aspect-video rounded-2xl bg-zinc-50 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950 dark:ring-zinc-800/50">
+//           <video
+//             src={src}
+//             autoPlay
+//             loop
+//             muted
+//             className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
+//           />
+//         </MorphingDialogContent>
+//         <MorphingDialogClose
+//           className="fixed top-6 right-6 h-fit w-fit rounded-full bg-white p-1"
+//           variants={{
+//             initial: { opacity: 0 },
+//             animate: {
+//               opacity: 1,
+//               transition: { delay: 0.3, duration: 0.1 },
+//             },
+//             exit: { opacity: 0, transition: { duration: 0 } },
+//           }}
+//         >
+//           <XIcon className="h-5 w-5 text-zinc-500" />
+//         </MorphingDialogClose>
+//       </MorphingDialogContainer>
+//     </MorphingDialog>
+//   )
+// }
 
 function MagneticSocialLink({
   children,
