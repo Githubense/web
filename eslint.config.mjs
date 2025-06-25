23cc6d1a9dd8c6141e9e8ae43ef3244b5ec6af75
Githubense <angelopf@outlook.com>
2025-06-25 14:51:33 +0200

    rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,11 @@ const eslintConfig = [
     "next/core-web-vitals",
     "next/typescript"
   ),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
